152ed7b47c55d9d6540c3f16e79bf60c78a8c960
Salim Ramirez <salimramirezm@gmail.com>
2025-07-27 14:32:41 -0500

    feat: refactor app.vue to integrate developer components and manage registration state

--- src/app.vue
+++ src/app.vue
@@ -1,31 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import {Developer} from "./greetings/model/developer.entity.js";
+import RegisterDeveloper from "./greetings/components/register-developer.component.vue";
+import ShowDeveloperCount from "./greetings/components/show-developer-count.component.vue";
+import GreetDeveloper from "./greetings/components/greet-developer.component.vue";
+
+export default {
+  name: 'app',
+  components: {
+    RegisterDeveloper,
+    ShowDeveloperCount,
+    GreetDeveloper
+  },
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      developerCount: 0,
+      hasRegistered: false
+    }
+  },
+  methods: {
+    updateDeveloperCount(developer) {
+      const dev = new Developer(developer.firstName, developer.lastName);
+      if (dev.fullName !== 'Unknown') this.developerCount++;
+    },
+    onDeveloperRegistered(developer) {
+      this.firstName = developer.firstName;
+      this.lastName = developer.lastName;
+      this.hasRegistered = true;
+      console.log("Developer registered:", developer);
+      this.updateDeveloperCount(developer);
+    },
+    onLater(developer) {
+      this.firstName = '';
+      this.lastName = '';
+      this.hasRegistered = false;
+      console.log("Registration deferred");
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <pv-button>Hola</pv-button>
+  <h1>Hello Vue Developer Application</h1>
+  <register-developer v-on:developer-registered="onDeveloperRegistered" v-on:later="onLater" />
+  <greet-developer v-if="hasRegistered" :first-name="firstName" :last-name="lastName" />
+  <show-developer-count :developer-count="developerCount" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
