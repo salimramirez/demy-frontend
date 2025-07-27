d17b93d5a5a1256db4bd09f85307fa8a8cf1099e
Salim Ramirez <salimramirezm@gmail.com>
2025-07-27 14:32:34 -0500

    feat: add greet-developer component to display personalized greetings

---
+++ src/greetings/components/greet-developer.component.vue
@@ -0,0 +1,29 @@
+<script>
+import {Developer} from "../model/developer.entity.js";
+
+export default {
+  name: "greet-developer",
+  props: {
+    firstName: String,
+    lastName: String
+  },
+  methods: {
+    getGreeting() {
+      if (this.isAnonymous()) return "Welcome Anonymous Developer";
+      const developer = new Developer(this.firstName, this.lastName);
+      return `Congrats ${developer.fullName}! Now you are a Vue Developer!`;
+    },
+    isAnonymous() {
+      return !this.firstName?.trim() || !this.lastName?.trim();
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ getGreeting() }}</p>
+</template>
+
+<style scoped>
+
+</style>
